4281cfe2ae2b1f09ce6ce0e80599f86adbec4e27
rem <relli_emmanuel_javier@dlsl.edu.ph>
2025-05-17 13:23:11 +0800

    Designing of Admin Page

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -2,16 +2,30 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import NavigationBar from './NavigationBar';
+import AuthNavigator from './AuthNavigator';
+import { useAuth } from '../context/AuthContext';
+import AdminNavigationBar from '../adminNavigation/AdminNavigationBar';
 
 const Stack = createStackNavigator();
 
 const AppNavigator = () => {
+    const { isAuthenticated, userType } = useAuth();
+
     return (
         <NavigationContainer>
             <Stack.Navigator screenOptions={{
                 headerShown: false,
             }}>
-                <Stack.Screen name="MainTabs" component={NavigationBar} />
+                {!isAuthenticated ? (
+                    // Auth Stack
+                    <Stack.Screen name="Auth" component={AuthNavigator} />
+                ) : userType === 'admin' ? (
+                    // Admin Stack
+                    <Stack.Screen name="AdminTabs" component={AdminNavigationBar} />
+                ) : (
+                    // User Stack
+                    <Stack.Screen name="MainTabs" component={NavigationBar} />
+                )}
             </Stack.Navigator>
         </NavigationContainer>
     );
